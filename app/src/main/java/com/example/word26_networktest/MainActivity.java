16d0210c82ea249f4395351e60740a861fa01d49
Your Name <you@example.com>
2018-07-23 14:21:33 +0800

    使用HttpURLConnection

---
+++ app/src/main/java/com/example/word26_networktest/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.word26_networktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                action();
+            }
+        });
+        textView=findViewById(R.id.text);
+    }
+    private void action(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection=null;
+                BufferedReader reader=null;
+                try {
+                    URL url=new URL("https://www.baidu.com");
+                    connection=(HttpURLConnection)url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(5000);
+                    connection.setReadTimeout(8000);
+                    InputStream is=connection.getInputStream();
+                    reader=new BufferedReader(new InputStreamReader(is));
+                    final StringBuilder stringBuilder=new StringBuilder();
+                    String line;
+                    while((line=reader.readLine())!=null){
+                        stringBuilder.append(line);
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(stringBuilder.toString());
+                        }
+                    });
+                    reader.close();
+                    is.close();
+                    connection.disconnect();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                        try {
+                            if(reader!=null) {
+                                reader.close();
+                            }
+                            if(connection!=null){
+                                connection.disconnect();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                }
+
+            }
+        }).start();
+    }
+}
